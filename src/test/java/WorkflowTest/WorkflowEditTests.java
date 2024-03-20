9227e20c40891c6f415ec4c02c5f2de2e7b63e1c
Abdulrahman <emadabdo258@gmail.com>
2024-03-20 13:10:46 +0200

    "Use Fluent"

--- src/test/java/WorkflowTest/WorkflowEditTests.java
+++ src/test/java/WorkflowTest/WorkflowEditTests.java
@@ -3,7 +3,6 @@ package WorkflowTest;
 import GlobalSetup.GlobalSetup;
 import LoginTest.LoginTests;
 import Pages.SideMenu;
-import Pages.WorkflowPages.EditWorkflowPage;
 import Pages.WorkflowPages.WorkflowDetails;
 import Pages.WorkflowPages.WorkflowsPage;
 import org.testng.Assert;
@@ -14,16 +13,13 @@ public class WorkflowEditTests extends GlobalSetup {
     LoginTests loginTests = new LoginTests();
     SideMenu sideMenu = new SideMenu(driver.getWebDriver());
     WorkflowsPage workflowsPage = new WorkflowsPage(driver.getWebDriver());
-    EditWorkflowPage editWorkflowPage = new EditWorkflowPage(driver.getWebDriver());
     WorkflowDetails workflowDetails = new WorkflowDetails(driver.getWebDriver());
-
     @BeforeClass
     public void checkLogin() {
         loginTests.testSuccessfulLogin();
         sideMenu.openWorkflowsPage().checkValidSearchResult("test flow1");
         workflowsPage.openEditPage().FillWorkflowPrimaryData("Supervisor");
     }
-
     @Test
     public void checkCreationWorkflowPageOpenedSuccessfully() {
         Assert.assertTrue(workflowDetails.checkPageTitle(), "In-valid title");
